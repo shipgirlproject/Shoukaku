a23e9ad263276319add7a26f077b8cc1e02cfe15
Deivu <davexdme@gmail.com>
2021-01-26 10:45:56 +0800

    fix: unnecessary rest due to wrong comparison

--- src/guild/ShoukakuPlayer.js
+++ src/guild/ShoukakuPlayer.js
@@ -368,7 +368,7 @@ class ShoukakuPlayer extends EventEmitter {
     async resume() {
         try {
             if (this.filters.equalizer.length) await this.setEqualizer(this.filters.equalizer);
-            if (this.volume !== 100) await this.setVolume(this.volume);
+            if (this.volume !== 1) await this.setVolume(this.volume);
             if (!this.track) {
                 this.emit('end', { type: 'ShoukakuResumeEvent', guildId: this.voiceConnection.guildID, reason: 'Tried to resume, but there is no track to use' });
             } else {
