42956b157fc47ad5b12283200270bd04da7afb16
Deivu <davexdme@gmail.com>
2021-03-07 16:42:08 +0800

    feat: add a debug log

--- src/guild/ShoukakuLink.js
+++ src/guild/ShoukakuLink.js
@@ -153,12 +153,13 @@ class ShoukakuLink extends EventEmitter {
     async setServerUpdate(data) {
         try {
             if (!data.endpoint) return;
+            this.node.emit('debug', this.node.name, `[Voice] Server Update Received => Node ${this.node.name}, Voice Server Moved? ${this.voiceMoved}`);
             clearTimeout(this.connectTimeout);
             this.voiceMoved = this.serverUpdate ? !data.endpoint.startsWith(this.region) : false;
             this.region = data.endpoint.split('.').shift().replace(/[0-9]/g, '');
             this.serverUpdate = data;
-            this.node.emit('debug', this.node.name, `[Voice] Forwarding Server Update => Node ${this.node.name}, Voice Server Moved? ${this.voiceMoved}`);
             await this.voiceUpdate();
+            this.node.emit('debug', this.node.name, `[Voice] Server Update Forwarded & Voice Connected => Node ${this.node.name}`);
             this.emit('ready');
         } catch (error) {
             this.authenticateFailed(error);
