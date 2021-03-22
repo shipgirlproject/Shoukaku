1db13783f3f9fb1cae24e803262b94d511f02d8d
112batman <tijnhoftijzer@gmail.com>
2021-03-22 07:10:56 +0100

    Add websocket header 'Num-Shards' on socket connect

--- src/node/ShoukakuSocket.js
+++ src/node/ShoukakuSocket.js
@@ -129,7 +129,8 @@ class ShoukakuSocket extends EventEmitter {
             'Client-Name': this.userAgent,
             'User-Agent': this.userAgent,
             'Authorization': this.auth,
-            'User-Id': id
+            'User-Id': id,
+            'Num-Shards': this.shoukaku.client.shard?.count || 1
         };
         if (resumable) headers['Resume-Key'] = (!!resumable).toString(); 
         this.emit('debug', this.name, 
@@ -138,7 +139,8 @@ class ShoukakuSocket extends EventEmitter {
             `  Client Name & User Agent     : ${this.userAgent}\n` +
             `  Authorization                : ${this.auth.split('').map((letter, index) => index === 0 ? letter : '*').join('')}\n` +
             `  User ID                      : ${id}\n` +
-            `  Resumable?                   : ${!!resumable}`
+            `  Resumable?                   : ${!!resumable}\n` +
+            `  Shard count                  : ${this.shoukaku.client.shard?.count || 1}`
         );
         this.ws = new Websocket(this.url, { headers });
         this.ws.once('upgrade', (...args) => this._upgrade(...args));
