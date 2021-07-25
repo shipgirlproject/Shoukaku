ed108e0a5459c4127c14b09caf04515795bd38dd
0t4u <61939142+0t4u@users.noreply.github.com>
2021-07-25 11:00:01 +0800

    any -> void

--- types/Utils.d.ts
+++ types/Utils.d.ts
@@ -8,5 +8,5 @@ export class Utils {
     public static getVersion(): { variant: 'light' | 'vanilla', version: string } | never;
     public static mergeDefault(def: Object, given: Object): Object;
     public static searchType(string: string): SearchTypes.soundcloud | SearchTypes.youtube | SearchTypes.youtubemusic;
-    public static wait(ms: number): Promise<any>;
+    public static wait(ms: number): Promise<void>;
 }
