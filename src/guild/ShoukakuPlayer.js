89d3041729b53487fd8a5fcf91c18c0936808642
Kakarot <kakarot.joel@gmail.com>
2021-08-04 11:01:51 +0530

    Missing ?

--- src/guild/ShoukakuPlayer.js
+++ src/guild/ShoukakuPlayer.js
@@ -25,7 +25,7 @@ class ShoukakuPlayer extends EventEmitter {
         this.connection = new ShoukakuConnection(this, node, options);
         /**
          * The track that is currently being played by this player
-         * @type {string}
+         * @type {?string}
          */
         this.track = null;
         /**
