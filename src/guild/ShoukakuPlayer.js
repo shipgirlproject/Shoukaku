5616f8d63767277978bcf3bca825e8ffa7e21f2a
Yimura <andreas.maerten@scarlet.be>
2021-09-04 11:37:14 +0200

    feat(Resume): Override ShoukakuPlayer#playTrack options when resuming (#67)
    * feat(Resume): Override options when resuming
    
    * feat(Typedef): Added playTrack options to resume
    
    * fix(Resume): Changed options param comment

--- src/guild/ShoukakuPlayer.js
+++ src/guild/ShoukakuPlayer.js
@@ -363,6 +363,7 @@ class ShoukakuPlayer extends EventEmitter {
     /**
      * Tries to resume your player, a use case for this is when you do ShoukakuPlayer.connection.attemptReconnect()
      * @memberOf ShoukakuPlayer
+     * @param {Object} [options={}] Used if you want to change noReplace, pause, put a custom track start or end time
      * @returns {ShoukakuPlayer}
      * @example
      * ShoukakuPlayer
@@ -370,9 +371,13 @@ class ShoukakuPlayer extends EventEmitter {
      *   .reconnect()
      *   .then(() => ShoukakuPlayer.resume());
      */
-    resume() {
+    resume(options = {}) {
         this.updateFilters();
-        if (this.track) this.playTrack(this.track, { startTime: this.position, pause: this.paused });
+        if (this.track) {
+            options = mergeDefault({ startTime: this.position, pause: this.paused }, options);
+
+            this.playTrack(this.track, options);
+        }
         this.emit('resumed');
         return this;
     }
