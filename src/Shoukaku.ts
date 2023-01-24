873c4aa15e77dac68c9a2722641b95a97b10df11
Saya <36309350+Deivu@users.noreply.github.com>
2023-01-24 20:50:50 +0800

    fix: typings for once and off

--- src/Shoukaku.ts
+++ src/Shoukaku.ts
@@ -136,21 +136,21 @@ export declare interface Shoukaku {
      * Emitted when a websocket connection to Lavalink disconnects
      * @eventProperty
      */
-    on(event: 'raw', listener: (name:string, json: unknown) => void): this;
+    on(event: 'raw', listener: (name: string, json: unknown) => void): this;
     once(event: 'reconnecting', listener: (name: string, reconnectsLeft: number, reconnectInterval: number) => void): this;
     once(event: 'debug', listener: (name: string, info: string) => void): this;
     once(event: 'error', listener: (name: string, error: Error) => void): this;
     once(event: 'ready', listener: (name: string, reconnected: boolean) => void): this;
     once(event: 'close', listener: (name: string, code: number, reason: string) => void): this;
     once(event: 'disconnect', listener: (name: string, players: Player[], moved: boolean) => void): this;
-    once(event: 'raw', listener: (json: any) => void): this;
+    once(event: 'raw', listener: (name: string, json: unknown) => void): this;
     off(event: 'reconnecting', listener: (name: string, reconnectsLeft: number, reconnectInterval: number) => void): this;
     off(event: 'debug', listener: (name: string, info: string) => void): this;
     off(event: 'error', listener: (name: string, error: Error) => void): this;
     off(event: 'ready', listener: (name: string, reconnected: boolean) => void): this;
     off(event: 'close', listener: (name: string, code: number, reason: string) => void): this;
     off(event: 'disconnect', listener: (name: string, players: Player[], moved: boolean) => void): this;
-    off(event: 'raw', listener: (json: any) => void): this;
+    off(event: 'raw', listener: (name: string, json: unknown) => void): this;
 }
 
 /**
