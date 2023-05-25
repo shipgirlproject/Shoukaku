56e71978be21d2de8464f6cddfac6680144969e6
flav <48295189+flav-code@users.noreply.github.com>
2023-05-25 17:58:03 +0200

    fix: typo on emit resume event (#149)

--- src/guild/Player.ts
+++ src/guild/Player.ts
@@ -549,7 +549,7 @@ export class Player extends EventEmitter {
         if (options.endTime) data.playerOptions.position;
         if (options.pause) data.playerOptions.paused = options.pause;
         await this.update(data);
-        this.emit('resume', this);
+        this.emit('resumed', this);
     }
 
     /**
