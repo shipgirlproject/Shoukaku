af8c3e5df5d1ceaa9f5269dbceba32b112b69802
0t4u <61939142+0t4u@users.noreply.github.com>
2024-08-10 13:58:37 +0800

    feat: use new eslint config format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,39 @@
 // @ts-check
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import * as augu from '@augu/eslint-config';
 
-import eslint from "@eslint/js";
-
-export default [
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    // @ts-expect-error incorrect type from package, but still compatible
+    augu.javascript(),
+    // temp disable due to rule move from typescript-eslint (ts) -> stylistic (style)
+    // await augu.typescript(),
     {
-        "parser": "@typescript-eslint/parser",
-        "extends": "@augu",
-        "rules": {
-            "indent": ["error", 4, { "SwitchCase": 1 }],
-            "quotes": ["error", "single"],
-            "brace-style": ["error", "1tbs"],
-            "array-bracket-spacing": ["error", "always"],
-            "block-spacing": ["error", "always"],
-            "arrow-spacing": "error",
-            "switch-colon-spacing": ["error", {"after": true, "before": false}],
-            "camelcase": "off",
-            "require-await": "error"
-        }
-    }
-];
+        ignores: [
+            'docs/',
+            'dist/',
+            'node_modules/',
+        ],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            'indent': [ 'error', 4, { 'SwitchCase': 1 } ],
+            'quotes': [ 'error', 'single' ],
+            'brace-style': [ 'error', '1tbs' ],
+            'object-curly-spacing': [ 'error', 'always' ],
+            'array-bracket-spacing': [ 'error', 'always' ],
+            'block-spacing': [ 'error', 'always' ],
+            'arrow-spacing': 'error',
+            'switch-colon-spacing': [ 'error', { 'after': true, 'before': false } ],
+            'camelcase': 'off',
+            'require-await': 'error',
+        },
+    },
+);
