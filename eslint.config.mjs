d799be95d0b8d5e3bdac0d64dceafc51e7cd11cf
0t4u <61939142+0t4u@users.noreply.github.com>
2024-08-10 16:14:33 +0800

    chore: eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
 import * as augu from '@augu/eslint-config';
 
 export default tseslint.config(
@@ -23,15 +24,28 @@ export default tseslint.config(
                 tsconfigRootDir: import.meta.dirname,
             },
         },
+        plugins: {
+            '@stylistic': stylistic,
+        },
         rules: {
-            'indent': [ 'error', 4, { 'SwitchCase': 1 } ],
-            'quotes': [ 'error', 'single' ],
-            'brace-style': [ 'error', '1tbs' ],
-            'object-curly-spacing': [ 'error', 'always' ],
-            'array-bracket-spacing': [ 'error', 'always' ],
-            'block-spacing': [ 'error', 'always' ],
-            'arrow-spacing': 'error',
-            'switch-colon-spacing': [ 'error', { 'after': true, 'before': false } ],
+            '@stylistic/semi': [ 'error' ],
+            '@stylistic/member-delimiter-style': [ 'error' ],
+            '@stylistic/indent': [ 'error', 4, { 'SwitchCase': 1 }],
+            '@stylistic/space-infix-ops': [ 'error' ],
+            '@stylistic/key-spacing': [ 'error', { 'mode': 'strict' }],
+            '@stylistic/keyword-spacing': [ 'error' ],
+            '@stylistic/indent-binary-ops': [ 'error', 4 ],
+            '@stylistic/type-generic-spacing': [ 'error' ],
+            '@stylistic/type-named-tuple-spacing': [ 'error' ],
+            '@stylistic/type-annotation-spacing': [ 'error', { 'before': false, 'after': true, 'overrides': { 'arrow': { 'before': true, 'after': true }}}],
+            '@stylistic/quotes': [ 'error', 'single' ],
+            '@stylistic/comma-dangle': [ 'error', 'always-multiline' ],
+            '@stylistic/brace-style': [ 'error', '1tbs' ],
+            '@stylistic/object-curly-spacing': [ 'error', 'always', { 'objectsInObjects': false, 'arraysInObjects': false }],
+            '@stylistic/array-bracket-spacing': [ 'error', 'always', { 'objectsInArrays': false, 'arraysInArrays': false }],
+            '@stylistic/block-spacing': [ 'error', 'always' ],
+            '@stylistic/arrow-spacing': 'error',
+            '@stylistic/switch-colon-spacing': [ 'error', { 'after': true, 'before': false }],
             'camelcase': 'off',
             'require-await': 'error',
         },
