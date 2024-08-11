e6ba242d6585dd490eee4922c7dd725eaced1caf
0t4u <61939142+0t4u@users.noreply.github.com>
2024-08-11 10:09:02 +0800

    fix: disable comma dangle

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -42,7 +42,7 @@ export default tseslint.config(
             '@stylistic/type-named-tuple-spacing': [ 'error' ],
             '@stylistic/type-annotation-spacing': [ 'error', { 'before': false, 'after': true, 'overrides': { 'arrow': { 'before': true, 'after': true }}}],
             '@stylistic/quotes': [ 'error', 'single' ],
-            '@stylistic/comma-dangle': [ 'error', 'always-multiline' ],
+            '@stylistic/comma-dangle': [ 'error', 'never' ],
             '@stylistic/brace-style': [ 'error', '1tbs' ],
             '@stylistic/object-curly-spacing': [ 'error', 'always', { 'objectsInObjects': false, 'arraysInObjects': false }],
             '@stylistic/array-bracket-spacing': [ 'error', 'always', { 'objectsInArrays': false, 'arraysInArrays': false }],
