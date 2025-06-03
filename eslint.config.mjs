e68d1301af801dfa901a33adb026055d29d7db16
0t4u <61939142+0t4u@users.noreply.github.com>
2025-06-03 11:32:29 +0100

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import config from '@shipgirl/eslint-config';
 
-export default [
-	...config(
-		import.meta.dirname,
-		{
-			rules: {
-				'import-x/extensions': 'off'
-			}
+export default config(
+	import.meta.dirname,
+	{
+		rules: {
+			'import-x/extensions': 'off'
 		}
-	)
-];
+	}
+);
