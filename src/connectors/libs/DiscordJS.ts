a1a205be74835f8954c0b7fdef108e45166186b4
Zeroknights <73501749+Zeroknights16@users.noreply.github.com>
2025-09-11 13:29:57 +0200

    Fixed ready event deprecation warning at v4 branch

--- src/connectors/libs/DiscordJS.ts
+++ src/connectors/libs/DiscordJS.ts
@@ -14,7 +14,7 @@ export class DiscordJS extends Connector {
 	// Listen attaches the event listener to the library you are using
 	public listen(nodes: NodeOption[]): void {
 		// Only attach to ready event once, refer to your library for its ready event
-		this.client.once('ready', () => this.ready(nodes));
+		this.client.once('clientReady', () => this.ready(nodes));
 		// Attach to the raw websocket event, this event must be 1:1 on spec with dapi (most libs implement this)
 		this.client.on('raw', (packet: any) => this.raw(packet));
 	}
